1a81d2268836184d580a3932603125fae6e3bca0
Samuel McManus <samuelmcmanus819@gmail.com>
2024-11-22 16:32:10 -0600

    Refine AI summarization process

--- src/background.ts
+++ src/background.ts
@@ -1,26 +1,29 @@
-const tldr = async(text: string) => {
+//If the text is too long to fit in the summarizer, then split it in half first
+const initialSummarization = async(text: string) => {
   if (text.length > 6000) {
-    const initialSummarizer = await ai.summarizer.create({ type: "tl;dr", length: 'long' });
-    text = await initialSummarizer.summarize(text);
-  }
+    const firstHalf = text.slice(0, text.length / 2);
+    const secondHalf = text.slice(text.length / 2);
+    const initialSummarizer = await ai.summarizer.create({ type: 'tl;dr', length: 'long' });
+    const firstHalfText = await initialSummarizer.summarize(firstHalf);
+    const secondHalfText = await initialSummarizer.summarize(secondHalf);
+    return await initialSummarizer.summarize(firstHalfText + secondHalfText);
+  } else { return text }
+}
+
+const tldr = async(text: string) => {
+  text = await initialSummarization(text);
   const summarizer = await ai.summarizer.create({ type: "tl;dr" });
   return await summarizer.summarize(text);
 }
 
 const eli5 = async(text: string) => {
-  if (text.length > 6000) {
-    const initialSummarizer = await ai.summarizer.create({ type: "tl;dr", length: 'long' });
-    text = await initialSummarizer.summarize(text);
-  }
+  text = await initialSummarization(text);
   const summarizer = await ai.summarizer.create({ type: "tl;dr", tone: 'formal', length: 'medium' , sharedContext: 'The reader is 5 years old'});
   return await summarizer.summarize(text)
 }
 
 const notes = async(text: string) => {
-  if (text.length > 6000) {
-    const initialSummarizer = await ai.summarizer.create({ type: "tl;dr", length: 'long' });
-    text = await initialSummarizer.summarize(text);
-  }
+  text = await initialSummarization(text);
   const summarizer = await ai.summarizer.create({ type: "key-points", format: 'plain-text' });
   return await summarizer.summarize(text)
 }
